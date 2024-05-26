3a8a20851fb9f0876e88d52a8dfc4e98c4bf567f
nesirid <nasirid@code.edu.az>
2024-05-27 00:53:13 +0400

    pagination

---
+++ Fiorello-PB101/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Fiorello_PB101.Helpers;
+using Fiorello_PB101.Services.Interfaces;
+using Fiorello_PB101.ViewModels.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello_PB101.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            var products = await _productService.GetAllPaginateAsync(page, 4);
+            var mappedDatas = _productService.GetMappedDatas(products);
+            int totalPage = await GetPageCountAsync(4);
+
+            Paginate<ProductVM> paginateDatas = new(mappedDatas, totalPage, page);
+            return View(paginateDatas);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts(int page = 1)
+        {
+            var products = await _productService.GetAllPaginateAsync(page, 4);
+            var mappedDatas = _productService.GetMappedDatas(products);
+            int totalPage = await GetPageCountAsync(4);
+
+            foreach (var product in mappedDatas)
+            {
+                product.MainImage = Url.Content($"~/img/{product.MainImage}");
+            }
+
+            Paginate<ProductVM> paginateDatas = new(mappedDatas, totalPage, page);
+            return Json(paginateDatas);
+        }
+
+        private async Task<int> GetPageCountAsync(int take)
+        {
+            int productCount = await _productService.GetCountAsync();
+            return (int)Math.Ceiling((decimal)productCount / take);
+        }
+    }
+}
